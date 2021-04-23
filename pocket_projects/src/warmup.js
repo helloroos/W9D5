65b2c91db6cf58215e31bf8381880f85aae8d53d
michelleroos <hello.michelleroos@gmail.com>
2021-04-23 13:56:26 -0400

    Finished warmp up and started working on pahse 1 of clock

--- pocket_projects/src/warmup.js
+++ pocket_projects/src/warmup.js
@@ -3,8 +3,8 @@ const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
 
-    let p = Document.createElement("p");
-    p.textContent(string);
+    let p = document.createElement("p");
+    p.textContent += string;
     htmlElement.appendChild(p);
 };
 
