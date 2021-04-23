3e45793cf45f145b3d5855ca68771153f268e7a8
Philip Lowe <philip.lowe94@gmail.com>
2021-04-23 13:32:05 -0400

    started warmup

--- pocket_projects/src/warmup.js
+++ pocket_projects/src/warmup.js
@@ -3,6 +3,9 @@ const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
 
+    let p = Document.createElement("p");
+    p.textContent(string);
+    htmlElement.appendChild(p);
 };
 
 htmlGenerator('Party Time.', partyHeader);
